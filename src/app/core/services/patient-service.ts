e336a279c6f306a0caa02de5ef98377bde719152
mili-emzz <m37009146@gmail.com>
2025-11-21 23:31:56 -0600

    get de doctor en user-profile

--- src/app/core/services/patient-service.ts
+++ src/app/core/services/patient-service.ts
@@ -18,28 +18,27 @@ export class PatientService {
   // /api para que funcione con el proxy
   createPatient(patient: Patient): Observable<PatientResponse> {
     if (environment.enableDebugMode) {
-      console.log('📤 Creando paciente con datos:', patient);
+      console.log('Creando paciente con datos:', patient);
     }    return this.http.post<PatientResponse>('/api/patient', patient);
   }
 
   getPatients(): Observable<PatientResponse> {
     if (environment.enableDebugMode) {
-      console.log('📥 Obteniendo lista de pacientes');
+      console.log('Obteniendo lista de pacientes');
     }
     return this.http.get<PatientResponse>('/api/patient');
   }
 
-  getPatientById(id: number): Observable<PatientResponse> {
-    if (environment.enableDebugMode) {
-      console.log('📥 Obteniendo paciente con ID:', id);
-    }
-    
-    return this.http.get<PatientResponse>(`/api/patient/${id}`);
-  }
+  // getPatientById(id: number): Observable<PatientResponse> {
+  //   if (environment.enableDebugMode) {
+  //     console.log('Obteniendo paciente con ID:', id);
+  //   }
+  //   return this.http.get<PatientResponse>(`/api/patient/${id}`);
+  // }
 
   deletePatient(id: number): Observable<any> {
     if (environment.enableDebugMode) {
-      console.log('🗑️ Eliminando paciente con ID:', id);
+      console.log('Eliminando paciente con ID:', id);
     }
     return this.http.patch(`/api/patient/${id}`, { statusId: 2 });
   }
