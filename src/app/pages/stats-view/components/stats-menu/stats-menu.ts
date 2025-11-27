93f4fa1223ebb19fc47055c7a425dd169c73a52f
lsprz2773 <243757@ids.upchiapas.edu.mx>
2025-11-26 20:21:12 -0600

    ruta modificada

--- src/app/pages/stats-view/components/stats-menu/stats-menu.ts
+++ src/app/pages/stats-view/components/stats-menu/stats-menu.ts
@@ -12,6 +12,7 @@ export class StatsMenu {
 
     onTypeChange(type: StatViewCardConfig['type']) {
       this.selectedType = type;
+      console.log(this.selectedType);
 
       //aqui pondre la logica del cambio de botones para los cambios de grafica
     }
