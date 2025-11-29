84fb91d8c0e760fb141ef2254dae1786f03632bd
lsprz2773 <243757@ids.upchiapas.edu.mx>
2025-11-28 22:38:18 -0600

    verificacion de estadisticas en vista de paciente

--- src/app/pages/patient/components/stats/stats.ts
+++ src/app/pages/patient/components/stats/stats.ts
@@ -1,5 +1,6 @@
 import {Component, Input} from '@angular/core';
 import {Router} from '@angular/router';
+import {PatientResponse} from '../../../../core/interfaces/patient';
 
 @Component({
   selector: 'app-stats',
@@ -9,8 +10,16 @@ import {Router} from '@angular/router';
 })
 export class Stats {
   @Input() patientId!: number;
+  @Input() patientData!: PatientResponse | null;
 
   constructor(private router:Router) {
+  }
+
+  verifyConsultations():boolean{
+    if (!this.patientData?.patient.consultations) {
+      return false;
+    }
+    return this.patientData.patient.consultations.length > 0;
   }
 
   onViewStats(){
