b884e7d7560838ca7339e0b531fd841aa05b4149
lsprz2773 <243757@ids.upchiapas.edu.mx>
2025-12-01 09:16:10 -0600

    intento de redireccion de stats

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,10 +1,10 @@
 export const environment = {
   AUTH_API_URL: '/api',
-  production: false,
-  apiUrl: '',
+  production: true,
   tokenKey: 'bp_token',
   doctorKey: 'bp_doctor',
+  apiUrl: 'http://ec2-98-95-251-108.compute-1.amazonaws.com:8080',
 
-  enableDebugMode: true,
-  logLevel: 'debug'
+  enableDebugMode: false,
+  logLevel: 'error'
 };
